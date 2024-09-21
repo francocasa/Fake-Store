da04dcdedec7fe6204b5592bd64379eb79b2d25c
Franco <20141674@aloe.ulima.edu.pe>
2024-09-21 03:25:46 -0500

    feat: inicio de contador

--- main.js
+++ main.js
@@ -4,17 +4,53 @@ async function listadoProductos() {
   try {
     const res = await fetch("https://fakestoreapi.com/products");
     const data = await res.json();
-    console.log(data);
+    data.forEach((element) => {
+      listado.appendChild(createCard(element.title, element.image));
+    });
   } catch (error) {
     console.log(error);
   }
 }
 listadoProductos();
 
-function createCard() {
-  const li = document.createElement("li");
-  li.innerHTML = "hola";
-  listado.appendChild(li);
+function createCard(title, image) {
+  const div__card = document.createElement("div");
+  div__card.className = "card";
+  /* Imagen */
+  const div__imagen_producto = document.createElement("div");
+  const imagen = document.createElement("img");
+  imagen.src = image;
+  div__imagen_producto.appendChild(imagen);
+  /* Descripción */
+  const div__descripcion_producto = document.createElement("div");
+  const descripcion = document.createElement("h3");
+  descripcion.innerHTML = title;
+  div__descripcion_producto.appendChild(descripcion);
+  /* Oferta */
+  const div__oferta_producto = document.createElement("div");
+  const temporizador = document.createElement("h3");
+  temporizador.innerHTML = "temporizador";
+  const boton = document.createElement("button");
+  boton.innerHTML = "boton";
+  div__oferta_producto.appendChild(temporizador);
+  div__oferta_producto.appendChild(boton);
+  /* Agregar divs */
+  div__card.appendChild(div__imagen_producto);
+  div__card.appendChild(div__descripcion_producto);
+  div__card.appendChild(div__oferta_producto);
+
+  return div__card;
 }
 
-createCard();
+const oferta_1min = Date.now() + 60 * 1000; //Aqui tomamos el tiempo de cuando queremos que el contador termine (En 60 segundos a partir de ahora)
+
+function mostrarTiempoTranscurrido() {
+  const tiempoTranscurrido = Math.floor((oferta_1min - Date.now()) / 1000);
+  if (tiempoTranscurrido > 0) {
+    console.log(`Han transcurrido ${tiempoTranscurrido} segundos`);
+    //Programamos para que se revise de nuevo el tiempo transcurrido en 500ms
+    setTimeout(mostrarTiempoTranscurrido, 1000);
+  } //else: Sino no programamos nada, es como si detuvieramos el timer
+}
+
+setTimeout(mostrarTiempoTranscurrido, 1000); //Mostramos el tiempo transcurrido (en este caso por consola, pero podría ser en cualquier lado)
